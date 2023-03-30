8094bbc4398f6992b8a7165ea51b9f02e44e47e1
jakub.tomiczek <tomiczekkuba01@gmail.com>
2023-03-30 23:43:22 +0200

    Add BigRational constans

--- BigRationalLib/BigRational.cs
+++ BigRationalLib/BigRational.cs
@@ -3,50 +3,41 @@ using System.Numerics;
 
 namespace BigRationalLib
 {
-    public readonly struct BigRational
+    public readonly partial struct BigRational
     {
-        public BigInteger Numerator { get; init; }
-        public BigInteger Denominator { get; init; }
+        public BigInteger Numerator { get; init; } = 0;
+        public BigInteger Denominator { get; init; } = 1;
+
+
+        #region constants
+
+        private static readonly string POSITIVE_INFINITY = "+Infinity";
+        private static readonly string NEGATIVE_INFINITY = "Infinity";
+        private static readonly string NAN = "NaN";
+
+        public static BigRational Zero { get; } = new(0);
+        public static BigRational One { get; } = new(1);
+        public static BigRational Half { get; } = new(1, 2);
+
+        public static BigRational NaN { get; } = default;
+        public static BigRational PositiveInfinity { get; } = new(1, 0);
+        public static BigRational NegativeInfinity { get; } = new(-1, 0);
+        #endregion
+
+
+        #region ctor's
 
         public BigRational(BigInteger numerator, BigInteger denominator)
         {
             Numerator = numerator;
             Denominator = denominator;
+
+
         }
 
-        public BigRational(string value)
-        {
-            if (value == null) throw new ArgumentNullException(nameof(value));
-
-            string[] array = value.Split('/');
-            if (array.Length != 2) throw new FormatException("Invalid fraction format. Expected numerator/denominator.");
-
-            if (BigInteger.TryParse(array[0], out BigInteger x))
-            {
-                Numerator = x;
-            }
-            else
-            {
-                throw new FormatException("Invalid numerator format.");
-            }
-
-            if (BigInteger.TryParse(array[1], out BigInteger y))
-            {
-                Denominator = y;
-            }
-            else
-            {
-                throw new FormatException("Invalid denominator format.");
-            }
-        }
-
-
-        public static readonly BigRational NaN = new(0, 0);
-        public static readonly BigRational Zero = new(0, 1);
-        public static readonly BigRational One = new(1, 1);
-        public static readonly BigRational Half = new(1, 2);
-       
-
-        public override string ToString() => $"{Numerator}/{Denominator}";
+        public BigRational(BigInteger value) 
+            : this(value, 1) 
+        {}
+        #endregion
     }
 }
