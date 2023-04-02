81ad7aa1dd7edffac246ff122a809076e3485627
jakubtomiczek <jakub.tomiczek@microsoft.wsei.edu.pl>
2023-04-02 19:14:54 +0200

    Final BigRational IComparable, IComparable<BigRational>

--- BigRationalLib/BigRationalRelations.cs
+++ BigRationalLib/BigRationalRelations.cs
@@ -1,16 +1,38 @@
+using System.Xml.Xsl;
+
 namespace BigRationalLib
 {
     public readonly partial struct BigRational : IComparable, IComparable<BigRational>
     {
         public int CompareTo(object? obj)
         {
-            throw new NotImplementedException();
+            if (obj is null) return +1;
+            if (obj is not BigRational) throw new ArgumentException();
+
+            var other = (BigRational)obj;
+            return this.CompareTo(other);
         }
 
         public int CompareTo(BigRational other)
         {
-            throw new NotImplementedException();
+            if (IsNaN(this) && !IsNaN(other)) return -1;
+            if (!IsNaN(this) && IsNaN(other)) return 1;
+            if (IsNaN(this) && IsNaN(other)) return 0;
+
+            if (IsNegativeInfinity(this)) return -1;
+            if (IsPositiveInfinity(other)) return 1;
+
+            return (this.Numerator * other.Denominator - this.Denominator * other.Numerator).Sign;
         }
+
+        public static bool operator <(BigRational left, BigRational right) 
+            => left.CompareTo(right) < 0;
+        public static bool operator >(BigRational left, BigRational right) 
+            => left.CompareTo(right) > 0;
+        public static bool operator <=(BigRational left, BigRational right) 
+            => left.CompareTo(right) <= 0;
+        public static bool operator >=(BigRational left, BigRational right) 
+            => left.CompareTo(right) >= 0;
     }
 }
 
