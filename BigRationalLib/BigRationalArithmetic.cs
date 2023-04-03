75a2e15711f2c82db276afce5967718d2e1c1912
jakubtomiczek <jakub.tomiczek@microsoft.wsei.edu.pl>
2023-04-03 22:27:03 +0200

    BigRationalArithmetic.cs #region Multiply

--- BigRationalLib/BigRationalArithmetic.cs
+++ BigRationalLib/BigRationalArithmetic.cs
@@ -5,7 +5,6 @@ namespace BigRationalLib
     public readonly partial struct BigRational
     {
         #region dodawanie
-        
         public BigRational Plus(BigRational other)
         {
             if (IsNaN(this) || IsNaN(other)) return NaN;
@@ -15,17 +14,7 @@ namespace BigRationalLib
             return new BigRational(this.Numerator * other.Denominator + this.Denominator * other.Numerator,
                                  this.Denominator * other.Denominator);
         }
-
-        public BigRational Minus(BigRational other)
-        {
-            if (IsNaN(this) || IsNaN(other)) return NaN;
-            if (IsPositiveInfinity(this) || IsPositiveInfinity(other)) return PositiveInfinity;
-            if (IsNegativeInfinity(this) || IsNegativeInfinity(other)) return NegativeInfinity;
-
-            return new BigRational(this.Numerator * other.Denominator - this.Denominator * other.Numerator,
-                this.Denominator * other.Denominator);
-        }
-
+        
         private static BigRational Sum(BigRational u1, BigRational u2) => u1.Plus(u2);
 
         public static BigRational Sum(BigRational u1, BigRational u2, params BigRational[]? list)
@@ -38,7 +27,24 @@ namespace BigRationalLib
 
             return sum;
         }
+        
+        public static BigRational operator +(BigRational u1, BigRational u2) => Sum(u1, u2);
+        public static BigRational operator ++(BigRational u) => u + One;
+        
+        #endregion dodawanie
 
+        #region odejmowanie
+        
+        public BigRational Minus(BigRational other)
+        {
+            if (IsNaN(this) || IsNaN(other)) return NaN;
+            if (IsPositiveInfinity(this) || IsPositiveInfinity(other)) return PositiveInfinity;
+            if (IsNegativeInfinity(this) || IsNegativeInfinity(other)) return NegativeInfinity;
+
+            return new BigRational(this.Numerator * other.Denominator - this.Denominator * other.Numerator,
+                this.Denominator * other.Denominator);
+        }
+        
         private static BigRational Subtract(BigRational u1, BigRational u2) => u1.Minus(u2);
 
         public static BigRational Subtract(BigRational u1, BigRational u2, params BigRational[]? list)
@@ -53,13 +59,7 @@ namespace BigRationalLib
 
             return sub;
         }
-
-        public static BigRational operator +(BigRational u1, BigRational u2) => Sum(u1, u2);
-        public static BigRational operator ++(BigRational u) => u + One;
-        #endregion dodawanie
-
-        #region odejmowanie
-
+        
         public static BigRational operator --(BigRational u) => u - One;
         public static BigRational operator -(BigRational u) => new (-1 * u.Numerator, u.Denominator);
         public static BigRational operator -(BigRational u1, BigRational u2) => Subtract(u1, u2);
@@ -68,7 +68,29 @@ namespace BigRationalLib
 
         #region mnozenie
 
+        public BigRational Multiply(BigRational other)
+        {
+            if (IsNaN(this) || IsNaN(other)) return NaN;
+            if (IsPositiveInfinity(this) || IsPositiveInfinity(other)) return PositiveInfinity;
+            if (IsNegativeInfinity(this) || IsNegativeInfinity(other)) return NegativeInfinity;
+
+            return new BigRational(this.Numerator * other.Numerator,
+                this.Denominator * other.Denominator);
+        }
+
+        private static BigRational Multiplication(BigRational u1, BigRational u2) => u1.Multiply(u2);
+        public static BigRational Multiplication(BigRational u1, BigRational u2, params BigRational[]? list)
+        {
+            if (list is null) return u1.Multiply(u2);
+            BigRational result = u1.Multiply(u2);
+
+            foreach (var u in list)
+                result = result.Multiply(u);
+
+            return result;
+        }
         
+        public static BigRational operator *(BigRational u1, BigRational u2) => Multiplication(u1, u2);
 
         #endregion
     }
