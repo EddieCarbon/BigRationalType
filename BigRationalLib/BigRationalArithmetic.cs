25b5beb707d3095445615e0e4fb2408c417c84bd
jakubtomiczek <jakub.tomiczek@microsoft.wsei.edu.pl>
2023-04-03 22:13:10 +0200

    BigRationalArithmetic.cs #region odejmowanie

--- BigRationalLib/BigRationalArithmetic.cs
+++ BigRationalLib/BigRationalArithmetic.cs
@@ -16,6 +16,16 @@ namespace BigRationalLib
                                  this.Denominator * other.Denominator);
         }
 
+        public BigRational Minus(BigRational other)
+        {
+            if (IsNaN(this) || IsNaN(other)) return NaN;
+            if (IsPositiveInfinity(this) || IsPositiveInfinity(other)) return PositiveInfinity;
+            if (IsNegativeInfinity(this) || IsNegativeInfinity(other)) return NegativeInfinity;
+
+            return new BigRational(this.Numerator * other.Denominator - this.Denominator * other.Numerator,
+                this.Denominator * other.Denominator);
+        }
+
         private static BigRational Sum(BigRational u1, BigRational u2) => u1.Plus(u2);
 
         public static BigRational Sum(BigRational u1, BigRational u2, params BigRational[]? list)
@@ -29,14 +39,36 @@ namespace BigRationalLib
             return sum;
         }
 
+        private static BigRational Subtract(BigRational u1, BigRational u2) => u1.Minus(u2);
+
+        public static BigRational Subtract(BigRational u1, BigRational u2, params BigRational[]? list)
+        {
+            if (list is null) return u1.Minus(u2);
+            BigRational sub = u1.Minus(u2);
+
+            foreach (var u in list)
+            {
+                sub = sub.Minus(u);
+            }
+
+            return sub;
+        }
+
         public static BigRational operator +(BigRational u1, BigRational u2) => Sum(u1, u2);
-        public static BigRational operator ++(BigRational u) => u + BigRational.One;
+        public static BigRational operator ++(BigRational u) => u + One;
         #endregion dodawanie
 
         #region odejmowanie
 
-        public static BigRational operator -(BigRational u) => new BigRational(-1 * u.Numerator, u.Denominator);
-        public static BigRational operator -(BigRational u1, BigRational u2) => 
+        public static BigRational operator --(BigRational u) => u - One;
+        public static BigRational operator -(BigRational u) => new (-1 * u.Numerator, u.Denominator);
+        public static BigRational operator -(BigRational u1, BigRational u2) => Subtract(u1, u2);
+        
+        #endregion
+
+        #region mnozenie
+
+        
 
         #endregion
     }
